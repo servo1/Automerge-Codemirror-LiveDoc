992d1ab97fcb3e5d6c23024cd7638f6ef2168f73
Martin Johnstone <martinejohnstone@gmail.com>
2019-05-31 18:03:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
 /**
  * Author: martinejohnstone@gmail.com
  * License: null
  */
-
 (function() {
 
-  var automerge = require('automerge');
+  const mc = require('mc');
 
   /**
    * LiveDoc - creates LiveDoc instance to coordinate changes
@@ -14,26 +12,35 @@
    *          This is intended to be a minimalist version of
    *          coordinating data between clients/servers
    *
-   * @param  {string} docId    Unique Document ID passed to send function as path
-   * @param  {object} cm       Code mirror instance already instantiated
-   * @param  {string} amdata   Results from Automerge.save() to initialize doc
-   * @param  {function} sendFunc Send function post CM changes
-   * @param  {object} emitter  Emitter for handling on(this.docId, changes) from server/client
-   * @return {object}          Instance of LiveDoc
+   * @param  {object} cmh    CodeMirrorHelper Instance
+   * @return {object}          Instance of CodeMirror
    */
-  function LiveDoc(docId, cm, amdata, sendFunc, emitter) {
+  function LiveDoc(cm) {
     this.cm = cm;
     this.cm.on('changes', this.cmChanges.bind(this));
-    this.docId = docId;
-    this.doc = automerge.load(amdata);
-    this.cm.getDoc().setValue(this.doc.text.join(''));
-    this.emitter = emitter;
-    this.send = sendFunc;
-    emitter.on(docId, this.applyToCM.bind(this));
-
+    mc.call(this);
     return this;
   }
+  inherits(mc, LiveDoc);
 
+  LiveDoc.prototype.init = function(docId, content) {
+    //this.cm.on('selectionChange', this.onSelectionChange.bind(this));
+    //this.cm.on('cursorActivity', this.onFocusOrActivity.bind(this));
+    //this.cm.on('focus', this.onFocusOrActivity.bind(this));
+    //this.cm.on('blur', this.onBlur.bind(this));
+    //this.cm.on('changes', this.onChanges.bind(this));
+    this.docId = docId;
+    if (typeof content === 'string') this.cmh.setContent(content);
+    else this.cm.getDoc().setValue('');
+  }
+
+  LiveDoc.prototype.off = function() {
+    this.cm.off('changes', this.cmChanges);
+  }
+
+  LiveDoc.prototype.handleErr = function(err) {
+    console.log(err, Error().stack);
+  }
 
   /**
    * LiveDoc.prototype.cmChanges - Take changes from CodeMirror instance for conversion
@@ -44,43 +51,15 @@
    */
   LiveDoc.prototype.cmChanges = function(cm, changes) {
     var i = 0,
-      len = changes.length;
+      len = changes.length,
+      change, diffChanges = [];
     for (; i < len; i++) {
-      this.cmChange(cm, changes[0]);
+      change = changes[i];
+      if (change.origin !== 'automerge' && change.origin !== 'setValue') {
+        this.getChange(changes[i], diffChanges);
+      }
     }
-  }
-
-  /**
-   * LiveDoc.prototype.cmChange - Handle one change from changes set
-   *
-   * @param  {object} cm     CodeMirror Instance
-   * @param  {object} change CM Change
-   * @return {undefined}        none but sends converted change data to send function provided to constructor of LiveDoc
-   */
-  LiveDoc.prototype.cmChange = function(cm, change) {
-    // && change.origin !== 'setValue'
-    if (change.origin !== 'automerge' && change.origin !== 'setValue') {
-      var newDoc = automerge.change(this.doc, this.applyChange.bind(this, change)),
-        //changes = automerge.getChanges(this.doc, newDoc),
-        nchanges = automerge.getChanges(this.doc, newDoc);
-      this.send(this.docId, {
-        operation: nchanges,
-        actorId: Automerge.getActorId(newDoc)
-      }, this.doRes.bind(this));
-      this.doc = newDoc;
-    }
-  }
-
-
-  /**
-   * LiveDoc.prototype.doRes - Handle response from send - blank as not necessary at this point
-   *   CRDT doesn't require handling a response/client/server
-   *
-   * @param  {type} data description
-   * @return {type}      description
-   */
-  LiveDoc.prototype.doRes = function(data) {
-
+    this.emit('changes', this.docId, diffChanges);
   }
 
 
@@ -91,19 +70,19 @@
    * @param  {object} doc    Automerge document
    * @return {undefined}
    */
-  LiveDoc.prototype.applyChange = function(change, doc) {
+  LiveDoc.prototype.getChange = function(change, diffChanges) {
     const at = this.cm.indexFromPos(change.from),
       removedLength = change.removed.join('\n').length,
-      addedText = change.text.join('\n');
+      addedText = change.text.join('\n'),
+      changes = [];
 
     if (removedLength > 0) {
-      doc.text.splice(at, removedLength)
+      diffChanges.push(['-', at, removedLength]);
     }
     if (addedText.length > 0) {
-      doc.text.insertAt(at, ...addedText.split(''))
+      diffChanges.push(['+', addedText, at]);
     }
   }
-
 
   /**
    * LiveDoc.prototype.applyToCM - Take changes from server/client and apply them to CM instance
@@ -114,17 +93,12 @@
    * @return {undefined}
    */
   LiveDoc.prototype.applyToCM = function(changes) {
-    var e, _a
-    var newDoc = automerge.applyChanges(this.doc, changes.operation.data),
-      nchanges = automerge.diff(this.doc, newDoc);
-    this.doc = newDoc;
     var i = 0,
-      len = nchanges.length;
+      len = changes.length;
     for (; i < len; i++) {
-      this.diffToCM(nchanges[i]);
+      this.diffToCM(changes[i]);
     }
   }
-
 
   /**
    * LiveDoc.prototype.diffToCM - Converts automerge.diff to codemirror changes
